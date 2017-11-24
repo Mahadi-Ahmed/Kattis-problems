5dee20b6f1aacdac75120d225e3c8afd47e6b776
MahadiPersonal <Mahadi4@gmail.com>
2017-11-25 00:26:41 +0100

    This program solves the problem but gets ValueError on kattis

--- R2/R2.py
+++ R2/R2.py
@@ -5,14 +5,23 @@
 # indata is 2 numbers: for example 11 15 where 11 is R1 and 15 is S1. 
 # (R1+R2)/2 = S1 find R2 -> R2 = (2*S1) - R1 
 
+import sys
+
 
 def main():
-	file = open("./samples/2.in", "r")
 	numbers = []
-	for f in file:
-		numbers.append(f)
+	for n in sys.stdin:
+		numbers.append(float(n))
+	R1 = numbers[0]
+	S1 = numbers[1]
+	R2 = float((2*S1) - (R1))
+	print int(R2)
 
-	print numbers
+
+
+	# calculate R1
+
+
 
 
 
