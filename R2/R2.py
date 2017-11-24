88b9890c425eb4864e40887a357ae1d36d6b7058
MahadiPersonal <Mahadi4@gmail.com>
2017-11-24 23:44:16 +0100

    Starting on problem R2

---
+++ R2/R2.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# OpenKattis problem R2
+# Mahadi 24-11-17
+
+# indata is 2 numbers: for example 11 15 where 11 is R1 and 15 is S1. 
+# (R1+R2)/2 = S1 find R2 -> R2 = (2*S1) - R1 
+
+
+def main():
+	file = open("./samples/2.in", "r")
+	numbers = []
+	for f in file:
+		numbers.append(f)
+
+	print numbers
+
+
+
+
+
+if __name__ == '__main__': main()
