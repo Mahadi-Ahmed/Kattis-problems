413f62c6caee775d78e39d48d2308bf309dc3118
Mahadi Ahmed <mahadi4@gmail.com>
2020-10-09 19:16:43 +0200

    cleaned up code & printing in sorted order

--- Costume-Contest/CostumeContest.java
+++ Costume-Contest/CostumeContest.java
@@ -8,21 +8,17 @@ class CostumeContest {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        String[] costumes = new String[size];
 
         HashMap<String,Integer> amountOfCostumes = new HashMap<String,Integer>();
-        
-        for (int i = 0; i < size; i++) {
-            costumes[i] = sc.next();
-        }
 
-        for (int i = 0; i < costumes.length; i++) {
-          if(amountOfCostumes.containsKey(costumes[i])) {
-              int incrementValue = amountOfCostumes.get(costumes[i]) + 1;
-              amountOfCostumes.put(costumes[i], incrementValue);
-          }  else {
-              amountOfCostumes.put(costumes[i], 1);
-          }
+        for (int i = 0; i < size; i++) {
+            String costume = sc.next();
+            if (amountOfCostumes.containsKey(costume)){
+                int incrementValue = amountOfCostumes.get(costume) + 1;
+                amountOfCostumes.put(costume, incrementValue);
+            } else {
+                amountOfCostumes.put(costume, 1);
+            }
         }
         
         // find min amount of costumes
@@ -33,29 +29,18 @@ class CostumeContest {
                 min = amountOfCostumes.get(i);
             }
         }
-        System.out.println("min value key " + min);
         
+        TreeSet<String> sortedCostumes = new TreeSet<String>();
+        // find winnning costume and order them
         for (String i : amountOfCostumes.keySet()) {
             if (amountOfCostumes.get(i) == min) {
-                System.out.println(i);
+                sortedCostumes.add(i);
             }
         }
-        
-        
-        //SortedSet<String> sortedCostumes = new TreeSet(amountOfCostumes.values());
-        // System.out.println(Arrays.toString(amountOfCostumes.values().toArray()));
-        // System.out.println("Sorted costumes: ");
-        // //System.out.println(Arrays.asList(sortedCostumes)); 
-        // Integer winner = Integer.parseInt(amountOfCostumes.values().toArray()[0].toString());
-        // System.out.println("Check if there are similar values");
-        // System.out.println(winner);
-        // System.out.println(amountOfCostumes.containsValue(amountOfCostumes.values().toArray()[0]));
-        // System.out.println(Arrays.asList(amountOfCostumes));
 
-        // loop over evert item in map and print key value
-
-        //System.out.println(Arrays.toString(costumes));
-
+        for (String i : sortedCostumes) {
+            System.out.println(i);
+        }
     }
 }
 
