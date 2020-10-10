14bd0e05aa1d162e0a85cb34e2d17411f176d7bd
Mahadi Ahmed <mahadi4@gmail.com>
2020-10-11 00:24:22 +0200

    fixed

--- two-sum/Solution.java
+++ two-sum/Solution.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    answer[i] = i;
-                    answer[j] = j;
-                }
-            }
-        }
-        return answer;
+      
+      int[] answer = new int[2];
+      
+      for (int i = 0; i < nums.length; i++) {
+          for (int j = i + 1; j < nums.length; j++) {
+              if (nums[i] + nums[j] == target) {
+                  answer[0] = i;
+                  answer[1] = j;
+              }
+          }
+         
+      }
+      
+      return answer;
+      
     }
 }
